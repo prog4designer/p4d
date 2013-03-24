4b3213357853b44d322857539af034f2e8275dd9
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-03-25 00:18:36 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+P4d::Application.config.secret_key_base = '5b70213eb8cc7e49e43736f60e9ce2eb813614ef04c372ea0c68540b32ff4b925be3a4c4432d90b72eb94d7150a6d72edfdbd9bfb4d896a1b986f3ae71a35b23'
